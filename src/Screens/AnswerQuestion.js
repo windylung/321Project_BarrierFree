8d222333c9a262aa4f81caf4089b94670c4c1235
windylung <jisoolee031@gmail.com>
2022-11-22 15:14:53 +0900

    feat Today's Question Screen

--- src/Screens/AnswerQuestion.js
+++ src/Screens/AnswerQuestion.js
@@ -1,23 +1,55 @@
-import React from "react";
-import { View, Text, Pressable, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Pressable, TextInput, StyleSheet } from "react-native";
 
 function AnswerQuestion({ navigation }) {
   //오늘의 질문, 답변 입력
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  const [text, setText] = useState("");
   return (
     //오늘의 질문
-    <View>
+    <View style={{ padding: 30 }}>
       <Text style={{ color: "grey" }}>#오늘의 질문</Text>
-      <Text>10. 01</Text>
-      <Text>상담을 시작한 목적이 무엇인가요?</Text>
+      <View
+        style={{
+          height: 177,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontWeight: "500", fontSize: 30 }}>
+          {month} . {date}
+        </Text>
+        <Text>상담을 시작한 목적이 무엇인가요?</Text>
+      </View>
       <View>
         <Text>나의 답변</Text>
-        <TextInput style={{
-            minHeight : 120,
-            backgroundColor : "grey"
-        }}></TextInput>
+        <TextInput
+          multiline
+          placeholder="답변을 입력해주세요"
+          style={styles.input}
+          value={text}
+          onChangeText={(payload) => setText(payload)}
+        ></TextInput>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    fontSize: 15,
+    Height: 150
+  },
+});
 
 export default AnswerQuestion;
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -3,14 +3,14 @@ import { View, Text, Pressable } from "react-native";
 
 function Home({ navigation }) {
   return (
-    <View>
-      <Pressable onPress={() => navigation.navigate("Question")}>
+    <View style={{flexDirection: "row"}}>
+      <Pressable style= {{paddingHorizontal: 20}} onPress={() => navigation.navigate("Question")}>
         <Text>문답</Text>
       </Pressable>
-      <Pressable onPress={() => navigation.navigate("Question")}>
+      <Pressable style= {{paddingHorizontal: 20}} onPress={() => navigation.navigate("Question")}>
         <Text>가족회의</Text>
       </Pressable>
-      <Pressable onPress={() => navigation.navigate("Question")}>
+      <Pressable style= {{paddingHorizontal: 20}} onPress={() => navigation.navigate("Question")}>
         <Text>정보제공</Text>
       </Pressable>
     </View>
