9e056d4d0195a9adea5864ef4a3ee980b4af1e03
windylung <jisoolee031@gmail.com>
2022-12-23 20:35:11 +0900

    feat. 가족회의

--- App.js
+++ App.js
@@ -5,20 +5,21 @@ import Home from './src/Screens/Home';
 import Question from './src/Screens/Question';
 import AnswerQuestion from './src/Screens/AnswerQuestion';
 import QuestionList from './src/Screens/QuestionList';
-import QuestionModal from './src/Screens/Modal';
 import AnswerList from './src/Screens/AnswerList';
+import MettingMain from './src/Screens/MettingMain';
 
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>{
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home"  screenOptions = {{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Question" component={Question} />
         <Stack.Screen name="AnswerQuestion" component={AnswerQuestion} />
         <Stack.Screen name="QuestionList" component={QuestionList} />
         <Stack.Screen name="AnswerList" component={AnswerList} />
+        <Stack.Screen name="MettingMain" component={MettingMain} />
         
       </Stack.Navigator>
     }</NavigationContainer>
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
-import { View, Text, Pressable, StyleSheet } from "react-native";
-import { COLOR_GREEN } from "../Color";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  S,
+  TouchableOpacitytyleSheet,
+  StyleSheet,
+} from "react-native";
+import { COLOR_BG, COLOR_GREEN } from "../Color";
 
 function Home({ navigation }) {
   //오늘 답변 작성 여부 (여기서 따질 필요는 없어보임)
   const [answer, setAnswer] = useState(true);
   return (
-    <View style={{flexDirection: "row", justifyContent: "center"}}>
-      <Pressable
-        style={mainStyle.btn}
-        onPress={() =>
-          navigation.navigate("QuestionList", { IsAnswer: { answer } })
-        }
-      >
-        <Text style={mainStyle.btnText}>문답</Text>
-      </Pressable>
-      <Pressable
-        style={mainStyle.btn}
-        onPress={() => navigation.navigate("Question")}
-      >
-        <Text style={mainStyle.btnText}>가족회의</Text>
-      </Pressable>
-      <Pressable
-        style={mainStyle.btn}
-        onPress={() => navigation.navigate("Question")}
-      >
-        <Text style={mainStyle.btnText}>정보제공!</Text>
-      </Pressable>
+    <View style={mainStyle.background}>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <TouchableOpacity
+          style={[mainStyle.btn, mainStyle.touchable]}
+          onPress={() =>
+            navigation.navigate("QuestionList", { IsAnswer: { answer } })
+          }
+        >
+          <Text style={mainStyle.btnText}>문답</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[mainStyle.btn, mainStyle.touchable]}
+          onPress={() => navigation.navigate("MettingMain")}
+        >
+          <Text style={mainStyle.btnText}>가족회의</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[mainStyle.btn, mainStyle.touchable]}
+          onPress={() => navigation.navigate("Question")}
+        >
+          <Text style={mainStyle.btnText}>정보제공</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const mainStyle = StyleSheet.create({
+export const mainStyle = StyleSheet.create({
+  background: {
+    padding: 30,
+    backgroundColor: COLOR_BG,
+    flex: 1,
+  },
   btn: {
     paddingHorizontal: 20,
     paddingVertical: 10,
@@ -39,11 +53,15 @@ const mainStyle = StyleSheet.create({
     width: 100,
     borderRadius: 10,
     marginHorizontal: 10,
-    marginVertical: 20
+    marginVertical: 20,
   },
   btnText: {
     textAlign: "center",
     fontSize: 15,
+  },
+  touchable: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
