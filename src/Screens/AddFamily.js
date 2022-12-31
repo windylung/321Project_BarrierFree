31effacb8be21d164f3e1ebdb993181e7c3f6ed0
windylung <jisoolee031@gmail.com>
2022-12-31 23:10:11 +0900

    fix. backend error

---
+++ src/Screens/AddFamily.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native"
+import { getUserInformation } from "./getUserInformation";
+import { ButtonContainer, SafeArea } from "./StyleComponent"
+
+export const AddFamily = () => {
+    // const [invitationID, setInvitationID] = useState(getUserInformation("InvitationID"));
+    // useEffect(() => {
+    //     // console.log(getUserInformation("role"));
+    //     const invitationID = getUserInformation("InvitationID");
+    //   }, []);
+
+    const ID = getUserInformation("invitationID");
+    console.log("ID", getUserInformation("invitationID"));
+    return (
+        <SafeArea>
+        <View>
+            <Text>설정 - 가족 추가</Text>
+            <Text>나의 코드{getUserInformation("invitationID")}</Text>
+            <Text>가족 코드{getUserInformation("familyID")}</Text>
+            <ButtonContainer>
+                <Text>연결된 가족 목록</Text>
+            </ButtonContainer>
+        </View>
+    </SafeArea>
+        );
+}
